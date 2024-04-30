b108dbf7d15ef8968b1eb5d3e3add34fdff6e811
Kzrtt <fe.hatunaqueton@gmail.com>
2024-04-30 13:32:51 -0300

    mudanças

--- index.php
+++ index.php
@@ -3,11 +3,12 @@ require_once 'vendor/autoload.php';
 
 use chillerlan\QRCode\QRCode;
 
-$qrcode = new QRCode();
+class QrCodeClass {
+    public function generateQrCode($data) {
+        $qrcode = new QRCode();
+        $qrCodeImage = $qrcode->render($data);
+        echo '<img width="350" height="350" src="' . $qrCodeImage . '" />';
+    }
+}
 
-// Dados que você quer codificar
-$data = '00020101021226820014br.gov.bcb.pix2560pix.stone.com.br/pix/v2/c309b65d-c12e-4ee2-9145-10f7732d561a52040000530398654040.015802BR5925Imaxis Administradora de 6014RIO DE JANEIRO62290525paclvlby3zfus761fk3d4cn6x6304EF1C';
 
-$qrCodeImage = $qrcode->render($data);
-
-echo '<img width="650" height="650" src="' . $qrCodeImage . '" />';
